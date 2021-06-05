8ea27a12d8d7b78b16064c1b7b0157869b12d5cd
Usuario <Usuario@LAPTOP-M2PQ13GV>
2021-06-05 20:19:14 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Esplai_S01_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey();//Para la ejecución hasta presionar tecla
+        }
+    }
+}
